cd5402e06f4e22976f0db088d35b2c07dbe313e4
SampleUser0001 <ittimfn+github@gmail.com>
2022-05-23 22:08:11 +0900

    Update copies.bat
    setlocal enabledelayedexpansionの重複を削除

--- copies.bat
+++ copies.bat
@@ -4,8 +4,6 @@ setlocal enabledelayedexpansion
 set PROJECT_HOME=c:\work\
 set PWD=%~dp0
 
-setlocal enabledelayedexpansion
-
 for /f "delims=" %%a in (%1) do (
     set origin_dir=%%~dpa
     mkdir !origin_dir:%PROJECT_HOME%=%PWD%!
